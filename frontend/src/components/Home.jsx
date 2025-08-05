8e99984da9144059177a3b923577fd9492fd210a
Vivek Vishwakarma <166617357+vivekv88@users.noreply.github.com>
2025-08-05 14:22:58 +0000

    h

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,18 +4,19 @@ function Home() {
   return (
     
     <div>
-        <div className='absolute z-10 ml-[15vw]'>
-            <img className='w-[10vw] cursor-pointer mb-[10vh]' src="/assets/logo1.png" alt="" />
+        <div className='flex justify-between w-[80vw] absolute z-1 ml-[15vw] font-serif items-center'>
+            <img className='w-[10vw] cursor-pointer' src="/assets/logo1.png" alt="" />
+            <button className='bg-gray-900 cursor-pointer rounded-lg text-[15px] text-cyan-100 h-[8vh] px-4 py-[0px]'>Register</button>
         </div>
-        <div  className='relative top-xl flex justify-center bg-cyan-900 h-[100vh] w-full font-serif'>
+        <div  className='relative flex justify-center bg-cyan-900 h-[100vh] w-full font-serif'>
             <div className='flex flex-col gap-4 items-center mt-[20vh] border-1 bg-transparent shadow-lg shadow-cyan-500 p-5 rounded-md h-[60vh] max-w-3xl'>
-            <h1 className='decoration-indigo-700 text-white mb-[40px]'>Login</h1>
+            <h1 className='decoration-indigo-700 text-amber-400 font-bold mb-[40px]'>Login</h1>
             <div className='flex flex-col w-sm'>
             <label htmlFor="email" className='text-white'>User Email</label>
             <input type="text" placeholder='Email' name='email' className='bg-transparent outline-none border-1 border-gray-400 px-3 py-2 rounded-lg mb-[30px] mt-[15px] text-gray-300 text-xl'/>
             <label className='text-white' htmlFor="password">Password</label>
             <input type="password" placeholder='Password' name='password' className='bg-transparent outline-none border-1 border-gray-400 px-3 py-2 rounded-lg mb-[30px] mt-[15px] text-gray-300 text-xl'/>
-            <button type='submit' className='px-3 py-2 bg-red-500 mb-[10px]'>Login</button>
+            <button type='submit' className='cursor-pointer text-white text-xl m-auto font-extrabold w-[10vw] rounded-lg px-3 py-2 bg-red-500 mb-[10px]'>Login</button>
         </div>
       </div>
         </div>
